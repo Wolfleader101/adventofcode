85b7c31cfd3a30fc201d3233d00e4f6fd92e29c2
Wolfleader <james.morgan@iinet.net.au>
2020-12-01 20:32:19 +0800

    Update sum.js

--- 2020/Day 1/sum.js
+++ 2020/Day 1/sum.js
@@ -2,12 +2,26 @@ const fs = require('fs')
 
 let numbers = fs.readFileSync('input.txt', 'utf-8').split('\n').map(Number)
 
+// 1
 numbers.forEach((num, i) => {
     numbers.forEach((secondNum, i) => {
         const sum = num + secondNum;
         if (sum == 2020) {
             console.log(num * secondNum)
         }
+    })
+});
+
+// 2
+numbers.forEach(num => {
+    numbers.forEach(secondNum => {
+        numbers.forEach(thirdNum => {
+            const sum = num + secondNum + thirdNum;
+            if (sum == 2020) {
+                console.log(num * secondNum * thirdNum)
+            
+            }
+        })
     })
 }); 
         
